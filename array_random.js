3bcae9db1463d4e4e59286a47db600cd50a10527
Dmitry <dmitry@gnuhost.eu>
2017-12-30 00:44:08 +0100

    chart ud

--- array_random.js
+++
@@ -1,7 +0,0 @@
-var array2 = [];
-var rand = Math.floor(Math.random() * array.length);
-
-for (var i = 1; i <= array.length; i++) {
-	array2[array2.length]= array[rand];
-}
-alert(array);
--- js/array_random.js
+++ js/array_random.js
@@ -1,7 +1,7 @@
-var array2 = [];
+var array2 = [0];
 
 for (var i = 1; i <= array.length; i++) {
 	var rand = Math.floor(Math.random() * array.length);
-	array2[array2.length]= array[rand];
+	array2[array2.length]= array2[array2.length-1] + array[rand];
 }
-alert(array2);
+document.querySelector('#array').innerHTML = array2
