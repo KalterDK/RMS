d5bccea0add97487956af83fb6d0f592dd4cfe54
Kalter <dmitry@gnuhost.eu>
2018-01-03 17:00:01 +0100

    chart modification

--- js/array_random.js
+++ js/array_random.js
@@ -1,7 +1,27 @@
-var array2 = [0];
+var array2 = ['data',0];
 
 for (var i = 1; i <= array.length; i++) {
 	var rand = Math.floor(Math.random() * array.length);
 	array2[array2.length]= array2[array2.length-1] + array[rand];
 }
-document.querySelector('#array').innerHTML = array2
+var array3 = [];
+array3[array3.length] = array2;
+
+//Second cycle
+for (var i = 1; i <= array.length; i++) {
+	var rand = Math.floor(Math.random() * array.length);
+	array2[array2.length]= array2[array2.length-1] + array[rand];
+}
+
+var array3 = [];
+array3[array3.length] = array2;
+
+//Third cycle
+for (var i = 1; i <= array.length; i++) {
+	var rand = Math.floor(Math.random() * array.length);
+	array2[array2.length]= array2[array2.length-1] + array[rand];
+}
+
+var array3 = [];
+array3[array3.length] = array2;
+// document.querySelector('#array').innerHTML = array2
